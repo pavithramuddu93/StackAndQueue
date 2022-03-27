87402f8efed1ecbc38dd44a2efc4e4d9cf09753a
pavithramuddu93 <pavithramuddu93>
2022-03-27 18:00:19 +0530

    UC2 added

--- src/com/stack/Queue.java
+++ src/com/stack/Queue.java
@@ -1,18 +1,19 @@
 package com.stack;
 
-    public class Queue<T> {
-        LinkedList<T> listObj = new LinkedList<T>();
+public class Queue<T> {
+    LinkedList<T> listObj = new LinkedList<T>();
 
-        public void enqueue(T value) {
-            listObj.add(value);
-        }
-        public void show() {
-            listObj.show();
-        }
-
-        public Node<T> dequeue() {
-            return listObj.pop();
-
-        }
+    public void enqueue(T value) {
+        listObj.add(value);
     }
+
+    public void show() {
+        listObj.show();
+    }
+
+    public Node<T> dequeue() {
+        return listObj.pop();
+
+    }
+}
 
--- src/com/stack/Stack.java
+++ src/com/stack/Stack.java
@@ -1,25 +1,24 @@
 package com.stack;
 
-    public class Stack<T> {
+public class Stack<T> {
 
-        LinkedList<T> listObj = new LinkedList<T>();
+    LinkedList<T> listObj = new LinkedList<T>();
 
+    public void push(T value) {
+        listObj.addAtStart(value);
+    }
 
-        public void push(T value) {
-            listObj.addAtStart(value);
-        }
+    public void show() {
+        listObj.show();
+    }
 
-        public void show() {
-            listObj.show();
-        }
-
-        public T peek() {
-            return listObj.peek();
-        }
+    public T peek() {
+        return listObj.peek();
+    }
 
         public Node<T> pop() {
-            return listObj.pop();
-
-        }
+        return listObj.pop();
 
     }
+
+}
